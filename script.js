d5d2bb39d05c996f7b7694abfb67eba6bfd3e0dc
carlos <junior.pessoaagradavel@gmail.com>
2023-05-02 20:33:18 -0300

    objetos

---
+++ script.js
@@ -0,0 +1,33 @@
+/* const pessoa ={
+    nome: 'carlos',
+    idade: '15',
+    diversao: 'adoro jogar volei'
+}
+
+console.log(`o nome da pessoa é ${pessoa.nome}, o(a) ${pessoa.nome} tem ${pessoa.idade} anos  e adora ${pessoa.diversao}`) */
+
+/* const professores =[
+    {nome: `andrei`, modulo: 1},
+    {nome: `pedro` , modulo: 2}
+]
+console.log(professores)
+console.log(professores[1])
+console.log(professores[2].nome) */
+
+//EXERCICIO 1
+
+const filme={
+    presidente:`alberto guerra`,
+    treinador: 'renato gaucho',
+    nome:'campeao brasileiro 2023',
+    ano: 2023,
+    elenco: ['grando','geromel','kenemann','luis soares'],
+    visualizado : 'N'
+}
+
+filme.personagens = ['donatelo', 'leonardo',
+'rafael', 'michelangelo'];
+
+console.log(filme.personagens[2])
+
+console.log(filme.elenco[0], 'como', filme.presidente[0] );
